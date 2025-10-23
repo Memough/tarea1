1fed06bc8f8c92006fe507f94a9687ef4c5b7872
Paul Adrian Rodriguez Hernandez <c39321@localhost.localdomain>
2025-10-23 10:33:05 -0600

    starting the trie stuff

--- src/tarea1/triepunteros.py
+++ src/tarea1/triepunteros.py
@@ -1,4 +1,62 @@
 from tarea1.diccionario import Diccionario
 
+class Nodo:
+    def __init__(self, elemento:str=''):
+        self.elemento = elemento
+        self.hijo: Nodo | None = None
+        self.siguiente: Nodo |None = None
+
+
 class TriePunteros(Diccionario):
-    pass
+    def __init__(self):
+        self.__raiz = Nodo()
+        self.__tamaño = 0
+
+    def __len__(self)->int:
+        return self.__tamaño
+    
+    #use later XDn't
+    def __charExist__(self, char:str, actual:Nodo)->int:
+        index = 0
+        while actual is not None:
+            if actual.elemento == str:
+                return index
+            else:
+                actual = actual.siguiente
+                index += 1
+        return index
+
+    #finnish later bruh
+    def inserte(self, elemento:str):
+        actual = self.__raiz
+        if self.tamaño == 0:
+            for n in elemento:
+                actual.raiz.hijo = Nodo(n)
+                actual = actual.hijo
+
+    #do later
+    def borre(self, elemento:str):
+        pass
+
+    def limpie(self):
+        self.__raiz.hijo = None
+        self.__tamaño = 0
+
+    #do later as well
+    def miembro(self, elemento:str)->bool:
+        pass
+
+    def imprima(self) -> None:
+        print(self)
+
+    #modify later
+    def __str__(self)->str:
+        elementos = []
+        actual = self.__raiz.hijo
+        while actual is not None:
+            elementos.append(actual.elemento)
+            actual = actual.hijo
+        return "\n".join(elementos)
+
+    def __del__(self):
+        self.limpie()
