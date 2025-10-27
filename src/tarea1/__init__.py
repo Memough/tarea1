357b97fc7bcedce7f4409a6875d163ddd26b1e72
MEMO <152151374+Memough@users.noreply.github.com>
2025-10-26 23:59:25 -0600

    ABB Listos

--- src/tarea1/__init__.py
+++ src/tarea1/__init__.py
@@ -20,6 +20,8 @@ from rich.prompt import Prompt
 from tarea1.diccionario import Diccionario
 from tarea1.listaordenadadinamica import ListaOrdenadaDinámica
 from tarea1.listaordenadaestatica import ListaOrdenadaEstática
+from tarea1.abbpunteros import ABBPunteros
+from tarea1.abbvectorheap import ABBVectorHeap
 from tarea1.triearreglos import TrieArreglos
 
 console = Console()
@@ -289,9 +291,9 @@ def menu_clase() -> Diccionario:
                 case "3":
                     return TablaHashAbierta()
                 case "4":
-                    pass
+                    return ABBPunteros()
                 case "5":
-                    pass
+                    return ABBVectorHeap()
                 case "6":
                     pass
                 case "7":
